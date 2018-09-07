4c2700dacc4badd7933ad93e95b600c86f0515ac
Niha Pereira <niha.pereira@gmail.com>
2018-09-06 20:29:08 -0500

    dddntwork

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 
 // Website you wish to allow to connect
 //res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+res.setHeader('Access-Control-Allow-Origin', '*');
 // Request methods you wish to allow
  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 // // Request headers you wish to allow
